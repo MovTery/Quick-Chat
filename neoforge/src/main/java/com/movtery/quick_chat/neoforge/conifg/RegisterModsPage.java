dba24d1819b2ec194f97702e65a96a71cdbecdd6
movte <212824502@qq.com>
2025-02-16 00:41:20 +0800

    1.21 - 1.21.1

--- neoforge/src/main/java/com/movtery/quick_chat/neoforge/conifg/RegisterModsPage.java
+++ neoforge/src/main/java/com/movtery/quick_chat/neoforge/conifg/RegisterModsPage.java
@@ -1,8 +1,8 @@
 package com.movtery.quick_chat.neoforge.conifg;
 
 import com.movtery.quick_chat.gui.ConfigScreen;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.Screen;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 import org.jetbrains.annotations.NotNull;
@@ -11,8 +11,8 @@ public class RegisterModsPage {
     public static void registerModsPage() {
         ModLoadingContext.get().registerExtensionPoint(IConfigScreenFactory.class, () -> new IConfigScreenFactory() {
             @Override
-            public @NotNull Screen createScreen(@NotNull Minecraft minecraft, @NotNull Screen screen) {
-                return new ConfigScreen(screen);
+            public @NotNull Screen createScreen(@NotNull ModContainer modContainer, @NotNull Screen arg) {
+                return new ConfigScreen(arg);
             }
         });
     }
